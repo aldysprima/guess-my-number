ddf4286139bb144b1c172edc89cf9cad435c8d8e
aldy <aldys.prima@gmail.com>
2021-12-07 14:15:42 +0700

    refactoring js code

--- script.js
+++ script.js
@@ -40,21 +40,6 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
-// Again Button
-// document.querySelector('.again').addEventListener('click', function () {
-//   score = 20;
-//   secretNumber = Math.trunc(Math.random() * 20) + 1;
-//   document.querySelector('.message').textContent = 'Start Guessing...';
-//   document.querySelector('.score').textContent = score;
-//   document.querySelector('number').textContent = '?';
-//   document.querySelector('guess').value = '';
-//   // document.querySelector('body').style.backgroundColor = '#222';
-//   // document.querySelector('.number').style.width = '15rem';
-
-//   document.querySelector('body').style.backgroundColor = '#222';
-//   document.querySelector('.number').style.width = '15rem';
-// });
-
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
