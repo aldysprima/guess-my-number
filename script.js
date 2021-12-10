02a20ebf0b4d9a93746059d4774c0514cf1eef4b
aldy <aldys.prima@gmail.com>
2021-12-10 10:17:36 +0700

    add minor changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-// let secretNumber = Math.trunc(Math.random() * 20) + 1;
-// let score = 20;
-// let highscore = 0;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
 
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
